319ce4430585977f7ca4f60ac5ff243b363fe4ba
corazon03 <82195740+corazon03@users.noreply.github.com>
2022-05-25 18:05:19 -0600

    Versión 1

--- index.php
+++ index.php
@@ -10,37 +10,39 @@
     <link href="style.css" rel="stylesheet"><link>
     <h1 align="center">Calculadora</h1>
     <div id="principal">
-        <table align="center">
-            <tr>
-                <td id="pantalla" colspan="5">0</td>
-            </tr>
-            <tr>
-                <td><button>7</button></td>
-                <td><button>8</button></td>
-                <td><button>9</button></td>
-                <td><button><</button></td>
-                <td><button>DEL</button></td>
-            </tr>
-            <tr>
-                <td><button>4</button></td>
-                <td><button>5</button></td>
-                <td><button>6</button></td>
-                <td><button>/</button></td>
-                <td><button>x</button></td>
-            </tr>
-            <tr>
-                <td><button>1</button></td>
-                <td><button>2</button></td>
-                <td><button>3</button></td>
-                <td><button>+</button></td>
-                <td><button>-</button></td>
-            </tr>
-            <tr>
-                <td><button>.</button></td>
-                <td><button>0</button></td>
-                <td colspan="3"><button id="igual">=</button></td>
-            </tr>
-        </table>
+        <form name="calculadora">
+            <table align="center">
+                <tr>
+                    <td colspan="5"><input id="pantalla" type="textfield" name="display" value=""></td>
+                </tr>
+                <tr>
+                    <td><input type="button" name="num" value="7" onClick="document.calculadora.display.value += '7'"></td>
+                    <td><input type="button" name="num" value="8" onClick="document.calculadora.display.value += '8'"></td>
+                    <td><input type="button" name="num" value="9" onClick="document.calculadora.display.value += '9'"></td>
+                    <td><button><</button></td>
+                    <td><input type="reset" name="ope" value="DEL"></td>
+                </tr>
+                <tr>
+                    <td><input type="button" name="num" value="4" onClick="document.calculadora.display.value += '4'"></td>
+                    <td><input type="button" name="num" value="5" onClick="document.calculadora.display.value += '5'"></td>
+                    <td><input type="button" name="num" value="6" onClick="document.calculadora.display.value += '6'"></td>
+                    <td><input type="button" name="ope" value="/" onClick="document.calculadora.display.value += '/'"></td>
+                    <td><input type="button" name="ope" value="x" onClick="document.calculadora.display.value += 'x'"></td>
+                </tr>
+                <tr>
+                    <td><input type="button" name="num" value="1" onClick="document.calculadora.display.value += '1'"></td>
+                    <td><input type="button" name="num" value="2" onClick="document.calculadora.display.value += '2'"></td>
+                    <td><input type="button" name="num" value="3" onClick="document.calculadora.display.value += '3'"></td>
+                    <td><input type="button" name="ope" value="+" onClick="document.calculadora.display.value += '+'"></td>
+                    <td><input type="button" name="ope" value="-" onClick="document.calculadora.display.value += '-'"></td>
+                </tr>
+                <tr>
+                    <td><input type="button" name="num" value="." onClick="document.calculadora.display.value += '.'"></td>
+                    <td><input type="button" name="num" value="0" onClick="document.calculadora.display.value += '0'"></td>
+                    <td colspan="3"><input type="button" name="num" value="=" onClick="document.calculadora.display.value += '='"></td>
+                </tr>
+            </table>
+        </form>
     </div>
 </body>
 </html>
